10fd3a578d8ae5a62ebf9ef6e5af30fff95544e1
Zhengyang Liu <Zhengyang.Liu@stonybrook.edu>
2016-10-25 16:34:30 -0400

    ops

--- scripts/Main.js
+++ scripts/Main.js
@@ -24,7 +24,7 @@ function selectIntro() {
     $("#part1").show();
 }
 
-function selectPractice() {}
+function selectPractice() {
     hint=1;
     $("#selectPanel").hide();
     $("#part2").show();
